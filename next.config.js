207ebc34e5f047ce18dab9b4c46b912a9e26f234
isabelamacedo <isabelacb.macedo@gmail.com>
2023-02-14 14:24:43 -0300

    feat: getting products from api

--- next.config.js
+++ next.config.js
@@ -1,6 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  experimental: {
+    newNextLinkBehavior: false,
+  },
+  images: {
+    domains: ['store.storeimages.cdn-apple.com'],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
